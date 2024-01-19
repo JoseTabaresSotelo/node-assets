b0227ec5950029969944140ed7c0bff4ea130ce4
Jose Tabares Sotelo <jose.tabares.sotelo@accenture.com>
2024-01-19 17:46:42 -0600

    Add comments

--- packages/fastify-postgres/src/app/plugins/db.ts
+++ packages/fastify-postgres/src/app/plugins/db.ts
@@ -2,6 +2,14 @@ import { FastifyInstance } from 'fastify';
 import fp from 'fastify-plugin';
 import pg from '@fastify/postgres';
 
+/** 
+* This is Data Base connection using postgres; register method support the coneccion
+* For local host use the follow environment variable DB_CONN_STRING
+* Sample: DB_CONN_STRING=postgres://postgres:pass@127.0.0.1:5432/northwind
+*
+* @see https://github.com/fastify/fastify-postgres 
+*
+*/
 export default fp(async function (fastify: FastifyInstance) {
   fastify.register(pg, {
     connectionString: process.env.DB_CONN_STRING,
--- packages/fastify-postgres/src/app/routes/categories/categories.ts
+++ packages/fastify-postgres/src/app/routes/categories/categories.ts
@@ -1,8 +1,26 @@
 import { runQuery } from '@api/db/utils';
 import { FastifyInstance, FastifyRequest } from 'fastify';
 
+
+/**
+ * Using the fastify-postgres allows to use SQL Postgres queries, this ones are working on Postgres workbenches and consoles
+ * Parameters in queries are expressed with `$1`, `$2` etc. This arguments are provided in runQuery method and passed in the query parameters
+ *  @see https://www.postgresql.org/docs/current/queries.html 
+ *  @see https://www.postgresql.org/docs/current/sql-syntax-calling-funcs.html
+ *  @see https://github.com/fastify/fastify-postgres 
+ */
+
+
 const getAllCategories = 'SELECT * FROM public.categories ORDER BY category_id ASC';
 const getCategoryById = 'SELECT * from public.categories WHERE category_id = $1';
+
+/**
+ * Creating fastify routes
+ *  The route methods will configure the endpoints of your application. You have two ways to declare a route with Fastify: the shorthand method and the full declaration.
+ *
+ * @see https://fastify.dev/docs/latest/Reference/Routes/
+ * 
+ */
 
 const categories = async (fastify: FastifyInstance) => {
   fastify.get('/categories', async () => {
