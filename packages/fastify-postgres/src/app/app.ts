53c40884666a7fd079b5fe8af341d4c285c3e490
Jose Tabares Sotelo <jose.tabares.sotelo@accenture.com>
2024-01-19 13:43:13 -0600

    Fix date in camelize

--- packages/fastify-postgres/src/app/app.ts
+++ packages/fastify-postgres/src/app/app.ts
@@ -7,7 +7,7 @@ const camelize = (obj) =>
   _.transform(obj, (acc, value, key, target) => {
     const camelKey = _.isArray(target) ? key : _.camelCase(key);
 
-    acc[camelKey] = _.isObject(value) ? camelize(value) : value;
+    acc[camelKey] = _.isObject(value) && !(value instanceof Date) ? camelize(value) : value;
   });
 
 /* eslint-disable-next-line */
