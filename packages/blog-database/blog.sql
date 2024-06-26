f656b6a40832363eb83803243849e8cf082fc263
Jose Tabares Sotelo <jose.tabares.sotelo@accenture.com>
2024-06-26 01:16:01 -0600

    Fix database creation

--- packages/blog-database/blog.sql
+++ packages/blog-database/blog.sql
@@ -13,8 +13,6 @@
 --     WITH
 --     OWNER = postgres
 --     ENCODING = 'UTF8'
---     LC_COLLATE = 'C.UTF-8'
---     LC_CTYPE = 'C.UTF-8'
 --     TABLESPACE = pg_default
 --     CONNECTION LIMIT = -1
 --     IS_TEMPLATE = False;
